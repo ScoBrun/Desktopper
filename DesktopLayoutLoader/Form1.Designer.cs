d4960aa42d380585a78d0a089c800b1615747602
Scott Brunton <scott.brunton366@gmail.com>
2020-09-28 20:19:41 +0100

    Add project files.

---
+++ DesktopLayoutLoader/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace DesktopLayoutLoader
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_save_layout = new System.Windows.Forms.Button();
+            this.btn_load_layout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_save_layout
+            // 
+            this.btn_save_layout.Location = new System.Drawing.Point(12, 12);
+            this.btn_save_layout.Name = "btn_save_layout";
+            this.btn_save_layout.Size = new System.Drawing.Size(100, 50);
+            this.btn_save_layout.TabIndex = 0;
+            this.btn_save_layout.Text = "Save Layout";
+            this.btn_save_layout.UseVisualStyleBackColor = true;
+            this.btn_save_layout.Click += new System.EventHandler(this.btn_save_layout_Click);
+            // 
+            // btn_load_layout
+            // 
+            this.btn_load_layout.Location = new System.Drawing.Point(127, 12);
+            this.btn_load_layout.Name = "btn_load_layout";
+            this.btn_load_layout.Size = new System.Drawing.Size(100, 50);
+            this.btn_load_layout.TabIndex = 0;
+            this.btn_load_layout.Text = "Load Layout";
+            this.btn_load_layout.UseVisualStyleBackColor = true;
+            this.btn_load_layout.Click += new System.EventHandler(this.btn_load_layout_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 75);
+            this.Controls.Add(this.btn_load_layout);
+            this.Controls.Add(this.btn_save_layout);
+            this.Name = "Form1";
+            this.Text = "Desktopper!";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_save_layout;
+        private System.Windows.Forms.Button btn_load_layout;
+    }
+}
+
---
+++ DesktopLayoutLoader/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopLayoutLoader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handler function for "Save Layout" button.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_save_layout_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            // Initial directory is "My Documents" folder.
+            sfd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            sfd.RestoreDirectory = true;
+
+            // Default extension for registration files.
+            sfd.DefaultExt = "reg";
+            sfd.Filter = "Registration Files (.reg)|*.reg";
+
+            // Set properties so user is prompted to create/overwrite if the registration file doesn't exist.
+            sfd.CreatePrompt = true;
+            sfd.OverwritePrompt = true;
+            sfd.AddExtension = true;
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                // TODO - DO SOMETHING!
+            }
+
+
+
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_load_layout_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
